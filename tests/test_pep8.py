4027786548ba5cf87c18a4e4d064acc0661c17c8
Jhon Arias <904@holbertonschool.com>
2019-11-07 16:54:03 -0500

    Add code to test pep8

--- tests/test_pep8.py
+++ tests/test_pep8.py
@@ -7,6 +7,6 @@ class TestCodeFormat(unittest.TestCase):
     def test_pep8_conformance(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['file1.py', 'file2.py'])
+        result = pep8style.check_files('test_models/*.py')
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
