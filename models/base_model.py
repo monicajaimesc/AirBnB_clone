3064c21223e7a02112c3560a3ea295a46ebcdec7
Jhon Arias <904@holbertonschool.com>
2019-11-07 17:15:28 -0500

    Create module and BaseModel class. Add docstrings

---
+++ models/base_model.py
@@ -0,0 +1,16 @@
+"""
+Module containing BaseModel class
+"""
+
+
+class BaseModel():
+    """
+    Defines all common attributes/methods for other classes. Parent class
+    
+    Attrs:
+        id (string): unique id. assign with an uuid when an instance is created
+        created_at (Date): current datetime when an instance is created
+        updated_at (Date): current datetime when an instance is updated
+    """
+    
+    pass
