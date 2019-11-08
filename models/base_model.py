a84c914f0e62cfa23f2934941c4b757ebee9ff58
monicajaimesc <monikmaja@gmail.com>
2019-11-08 19:36:58 +0000

    BaseModel from dictionary created

--- models/base_model.py
+++ models/base_model.py
@@ -16,13 +16,19 @@ class BaseModel():
         updated_at (Date): current datetime when an instance is updated
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
-        Method call when an instance is created
+        Intialize object class with 2 arguments/parameters
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(args) > 0:
+            for key in args[0]:
+                setattr(self, key, args[0][key])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        for key in kwargs:
+            print("kwargs: {}: {}".format(key, kwargs[key]))
 
     def __str__(self):
         """
