0c01664f718d6d600e01b0ef5d95533e236fbd30
Jhon Arias <jhonarmont@hotmail.com>
2019-11-08 20:40:20 -0500

    Task 4 completed. We use kwargs for constructor each key of this dictionary is an attribute name each value of this dictionary is the value of this attribute name. created_at and updated_at are strings in kwargs,  convert these strings into datetime object.

--- models/base_model.py
+++ models/base_model.py
@@ -16,13 +16,24 @@ class BaseModel():
         updated_at (Date): current datetime when an instance is updated
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
-        Method call when an instance is created
+        Method call when the instance is created
+        Sets all atributes. Created_at and updated_at are datetime objs
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
