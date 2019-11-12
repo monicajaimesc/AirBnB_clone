36e96c7a5db380a69b72d7464d717d516d7c6571
monicajaimesc <monikmaja@gmail.com>
2019-11-12 18:38:38 +0000

    working in file 7

--- console.py
+++ console.py
@@ -4,7 +4,9 @@ Implementing the consola
 """
 
 import cmd
-
+import json
+# from models.base_model import BaseModel
+# base = {'BaseModel': Basemodel}
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -31,10 +33,43 @@ class HBNBCommand(cmd.Cmd):
         """
         return True
     
+    def do_create(self, *args):
+        """
+        Creates a new instance of BaseModel, saves it (to the JSON file)
+        """
+        print(args[0])
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        try:
+            args = split(args)
+            new_instance = eval(args[0])()
+            new_instance.save()
+            print(new_instance.id)
+        
+        except:
+            print("** class doesn't exist **")
+        
+    def do_show(self, *args)
+        """
+        Prints the string representation 
+        of an instance based on the class name and id
+        """
+        args = split(args)
+        if len(args) == 0:
+            print("** class name missing **")
 
-if __name__  ==  '__main__' : 
-    """
-        Entry point for the loop
-    """
-    HBNBCommand () . cmdloop ()
+        if len(args) == 1:
+            print()
 
+
+if __name__ == "__main__":
+    '''
+        Entry point for the loop.
+    '''
+    HBNBCommand().cmdloop()
+
+
+
+
+   
