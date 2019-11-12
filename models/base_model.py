b7bd084531fcb3ad465171928c96d3ddf12ec7d9
Jhon Arias <jhonarmont@hotmail.com>
2019-11-12 04:15:07 -0500

    Change import.
    Import models package instead storage class.
    This way when we will call storage only when needed and not before

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 Module containing BaseModel class
 """
 import uuid
-from . import storage
+import models
 from datetime import datetime
 
 
@@ -24,7 +24,7 @@ class BaseModel:
         """
         if kwargs:
             for key, value in kwargs.items():
-                if key == "update_at":
+                if key == "updated_at":
                     self.updated_at = datetime.strptime(
                         value, "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "created_at":
@@ -36,7 +36,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -56,7 +56,8 @@ class BaseModel:
         datetime
         """
         setattr(self, 'updated_at', datetime.now())
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """
