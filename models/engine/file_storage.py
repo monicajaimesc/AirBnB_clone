e93926bd4e3bcac7812ad97152e399ede273a0fb
Jhon Arias <jhonarmont@hotmail.com>
2019-11-12 04:35:00 -0500

    Fix task5
    the attributes are now class attributes therefore I removed init method
    
    new method now stores the obj itself and not its dictionary
    
    save method calls to_dict method of each obj to get a serializable str
    
    reload method calls BaseModel with the kwargs gotten from json file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,60 +3,60 @@
 Declarates FileStorage class
 """
 import json
+import models
 
 
 class FileStorage:
-    """
-    will convert the dictionary representation to a JSON string.
-    With this format, humans can read and all programming languages have a
-    JSON reader and writer.
+	"""
+	will convert the dictionary representation to a JSON string.
+	With this format, humans can read and all programming languages have a
+	JSON reader and writer.
 
-    Basically it will store the json to a file and deserialize
-    that json to an obj, this way we restore all the data
-    of the program. JSON files work as our database
-    """
+	Basically it will store the json to a file and deserialize
+	that json to an obj, this way we restore all the data
+	of the program. JSON files work as our database
+	"""
 
-    def __init__(self):
-        """
-        Initialize instance with private attributes
-        """
-        self.__file_path = 'file.json'
-        self.__objects = {}
+	__file_path = 'file.json'
+	__objects = {}
 
-    def all(self):
-        """
-        returns __object dictionary with all objs saved on file.json
+	def all(self):
+		"""
+		returns __object dictionary with all objs saved on file.json
 
-        Returns:
-            (dict): dictionary with all objs. Key of obj is "<class name>.id",
-                    value is its .to_dict dictionary
-        """
-        return self.__objects
+		Returns:
+			(dict): dictionary with all objs. Key of obj is "<class name>.id",
+					value is its .to_dict dictionary
+		"""
+		return self.__objects
 
-    def new(self, obj):
-        """
-        sets in __objects the obj.to_dict with key <obj class name>.id
+	def new(self, obj):
+		"""
+		sets in __objects the obj.to_dict with key <obj class name>.id
 
-        to_dict returns a dictionary that is already in a format
-        that can be dump
-        """
-        key = obj.__class__.__name__ + '.' + obj.id
-        self.__objects[key] = obj.to_dict()
+		to_dict returns a dictionary that is already in a format
+		that can be dump
+		"""
+		key = obj.__class__.__name__ + '.' + obj.id
+		self.__objects[key] = obj
 
-    def save(self):
-        """
-        Saves to a file the JSON representation of __objects dictionary
-        """
-        with open(self.__file_path, 'w', encoding="utf-8") as file:
-            json.dump(self.__objects, file, indent="\t")
+	def save(self):
+		"""
+		Saves to a file the JSON representation of __objects dictionary
+		"""
+		with open(self.__file_path, 'w', encoding="utf-8") as file:
+			dic_objects = {key:value.to_dict() for key, value in self.__objects.items()}
+			json.dump(dic_objects, file, indent="\t")
 
-    def reload(self):
-        """
-        Converts the JSON string to a python obj
-        and sets __objects with it
-        """
-        try:
-            with open(self.__file_path, 'r', encoding="utf-8") as file:
-                self.__objects = json.load(file)
-        except Exception:
-            pass
+	def reload(self):
+		"""
+		Converts the JSON string to a python obj
+		and sets __objects with it
+		"""
+		try:
+			with open(self.__file_path, 'r', encoding="utf-8") as file:
+				json_objects = json.load(file)
+				for key, value in json_objects.items():
+					self.__objects[key] = models.BaseModel(**value)
+		except Exception:
+			pass
