b6688b2affce1322b3e2078653ec2d8f4f4103b3
Jhon Arias <jhonarmont@hotmail.com>
2019-11-13 03:48:31 -0500

    Update HBNBCommand class
    Import shlex module to split args correctly.
    
    Create do_update method to update obj based on the class name and id.
    It addes or updates attributes

--- console.py
+++ console.py
@@ -7,6 +7,8 @@ import cmd
 import json
 from models.base_model import BaseModel
 import models
+import shlex
+
 # base = {'BaseModel': Basemodel}
 
 class HBNBCommand(cmd.Cmd):
@@ -98,11 +100,59 @@ class HBNBCommand(cmd.Cmd):
 			if args[0] in models.classes.keys():
 				# check if id was passed
 				if len(args) >= 2:
-					for key, value in models.storage.all().items():
+					for key in models.storage.all().keys():
 						id_object = key.split('.')[1]
 						if args[1] == id_object:
 							del models.storage.all()[key]
 							models.storage.save()
+							break
+					else:
+						print("** no instance found **")
+				else:
+					print('** instance id missing **')
+			else:
+				print("** class doesn't exist **")
+
+	def do_update(self, args):
+		"""
+		Updates an instance based on the class name and id by adding
+		or updating attribute (save the change into the JSON file).
+		
+		Ex: $ update BaseModel 1234-1234-1234 email "aibnb@holbertonschool.com"
+		"""
+
+		args = shlex.split(args)
+		if len(args) == 0:
+			print('** class name missing **')
+		else:
+			# check if class exists
+			if args[0] in models.classes.keys():
+				# check if id was passed
+				if len(args) >= 2:
+					# check if istance exists
+					for key, value in models.storage.all().items():
+						id_object = key.split('.')[1]
+						if args[1] == id_object:
+							instance = value
+							# check if attribute name exists
+							if len(args) >= 3:
+								# check if attribute new value exists
+								if len(args) >= 4:
+									# get attribute type to cast new value
+									try:
+										attr_type = type(getattr(instance, args[2]))
+										setattr(instance, args[2], attr_type(args[3]))
+									except AttributeError:
+										# if attribute does not exists then I set a new attribute
+										# with the value passed
+										setattr(instance, args[2], args[3])
+
+									# Save changes
+									models.storage.save()	
+								else:
+									print('** value missing **')
+							else:
+								print('** attribute name missing **')
 							break
 					else:
 						print("** no instance found **")
