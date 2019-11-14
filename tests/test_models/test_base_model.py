9d27aee81f5f2f363657b5941b61aec81ff4acb6
monicajaimesc <monikmaja@gmail.com>
2019-11-14 00:08:15 +0000

    last test added for base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,14 +6,13 @@ import unittest
 from models.base_model import BaseModel
 from datetime import datetime
 
+
 class TestBaseModel(unittest.TestCase):
     """Test for Base Model"""
-    
-    @classmethod
-    # setUpClass class method called before tests in an 
+
+    # setUpClass class method called before tests in an
     # individual class run
-    
- 
+    @classmethod
     def setUpClass(cls):
         """
             defines all common attributes
@@ -25,7 +24,7 @@ class TestBaseModel(unittest.TestCase):
         cls.base2 = BaseModel()
         cls.base2.name = "Betty"
         cls.base2.my_number = 98
-        
+
     @classmethod
     def tearDownClass(cls):
         """
@@ -34,34 +33,54 @@ class TestBaseModel(unittest.TestCase):
         """
         pass
 
-    
-
     def test_docs(self):
         """Documentation of everithing"""
-        # All your modules should have a documentation 
+        # All your modules should have a documentation
         self.assertTrue(BaseModel.__doc__)
         # All your classes should have a documentation
         self.assertTrue(BaseModel.save.__doc__)
         # All your functions (inside and outside a class)
         # should have a documentation
         self.assertTrue(BaseModel.to_dict.__doc__)
-        
+
     def test_basic_functionality(self):
         """Test basic functionality of class"""
         # Test instance creation
         self.assertIsInstance(self.base1, BaseModel)
-        # hasattr: Return whether the object has an 
+        # hasattr: Return whether the object has an
         # attribute with the given name.
         self.assertTrue(hasattr(self.base1, "id"))
-        # id != name 
+        # id != name
         self.assertNotEqual(self.base1, self.base2)
         # when an instance is created
         # the time is update
-        #self.assertEqual(self.base1.created_at,
-        self.assertEqual(type(getattr(self.base1, 'created_at', None)), datetime)
+        # self.assertEqual(self.base1.created_at,
+        self.assertEqual(type(getattr(self.base1, 'created_at', None)),
+                         datetime)
 
-        
+    def test_save(self):
+        """
+        Test if an instance can be saved
+        """
+        # instance saved
+        self.base1.save()
+        # instance created and time update
+        self.assertNotEqual(self.base1.created_at, self.base1.updated_at)
 
+    def test_to_dict(self):
+        """
+        Test that a dictionary containing all keys/values of __dict__ of the
+        instance that was returned
+        """
+        # acces to the instance dictionary
+        # return all the keys values of __dict__
+        # of the instance
+        base1_dict = self.base1.to_dict()
+        # is equal the class name
+        self.assertEqual(self.base1.__class__.__name__, "BaseModel")
+        # isinstance(a, b)
+        self.assertIsInstance(base1_dict["created_at"], str)
+        self.assertIsInstance(base1_dict["updated_at"], str)
 
 if __name__ == '__main__':
     unittest.main()
